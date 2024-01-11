5ceb60eca5d003d6e7ac185898f4f8412e7c678f
Karina <150185334+Enmadnessgine@users.noreply.github.com>
2024-01-11 15:40:16 +0200

    Update urls.py

--- music_criticism/mainapp/urls.py
+++ music_criticism/mainapp/urls.py
@@ -3,15 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name = "index"),
-    #path('release/<int:pk>/', views.ReleaseDetailView.as_view(), name = 'release-detail'),
-    #path('release/<int:release_id>/add_comment/', views.add_comment, name='add_comment'),
     path('login/', views.user_login, name = 'login'),
     path('logout/', views.user_logout, name = 'logout'),
     path('signin/', views.signin, name = 'signin'),
-    #path('add-comment/', views.add_comment, name='add_comment')
-
     path('release/<int:release_id>/', views.release_detail, name='release_detail'),
-    #path('add-comment/<int:release_id>/', views.add_comment, name='add_comment'),
     path('release/<int:release_id>/add-comment/', views.add_comment, name='add_comment'),
     
 ]
